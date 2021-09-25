e07979a2f99570769ac835d5172a46f7867c6376
andreassenmarvin <marvinandreassen@gmail.com>
2021-09-25 22:47:46 +0300

    Create success route

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,8 @@
+import models.Post;
+
+
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
-
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,6 +15,13 @@ public class App {
         get("/", (request, response) -> {
             Map<String ,Object> model = new HashMap<String, Object>();
             return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post("/posts/new", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            String content = request.queryParams("content");
+            Post newPost = new Post(content);
+            return new ModelAndView(model, "success.hbs");
         }, new HandlebarsTemplateEngine());
     }
 }
